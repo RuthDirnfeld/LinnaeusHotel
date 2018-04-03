4456a2cae0201cca9bd2adf841febfb0b9df6450
Bruthzilla <ruth.dirnfeld@gmail.com>
2018-04-03 14:48:36 +0200

    Initial commit

---
+++ src/model/Reservation.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.util.Date;
+
+public class Reservation {
+	private int id;
+	private Guest guest;
+	private Room room;
+	private Date startDate;
+	private Date endDate;
+
+	public Reservation(int id, Guest guest, Room room, Date startDate, Date endDate) {
+		this.id = id;
+		this.guest = guest;
+		this.room = room;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Guest getGuest() {
+		return guest;
+	}
+
+	public void setGuest(Guest guest) {
+		this.guest = guest;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+}
