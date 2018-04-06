64e1f4abceb3a202898866716c584d0050b8336d
Matus Maruna <MatusMaruna@users.noreply.github.com>
2018-04-06 20:39:09 +0200

    Small edit
    added a list for guests in controller

--- src/controller/GuestController.java
+++ src/controller/GuestController.java
@@ -1,11 +1,18 @@
 package controller;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import model.Guest;
 
 public class GuestController {
+List<Guest> guestList = new ArrayList<Guest>();
 	
 public void createGuest(String name, String birthDate, String company, String citizenship, String address, boolean smoker, String email, String phoneNum, String favRoom, String creditNumber) {
 Guest g = new Guest(name,birthDate,company,citizenship,address,smoker,email, phoneNum,favRoom,creditNumber);
 System.out.println(g.toString());
+guestList.add(g);
+
 }
 }
